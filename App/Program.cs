7c1e2585c9734c865727727542d88be9e12fd4b1
Chintan joshi <cjoshi1123@gmail.com>
2025-04-18 00:37:04 -0400

    Behavioral design patterns - Command Pattern example

---
+++ App/Program.cs
@@ -0,0 +1,19 @@
+﻿using Command;
+var remote = new Remote();
+
+remote.SetCommand(new LightOnCommand(new Light()));
+remote.SendCommand();
+
+remote.SetCommand(new FanOnCommand(new Fan()));
+remote.SendCommand();
+
+remote.SetCommand(new LightOffCommand(new Light()));
+remote.SendCommand();
+
+remote.SetCommand(new FanOffCommand(new Fan()));
+remote.SendCommand();
+
+remote.SendUndoCommand();
+remote.SendUndoCommand();
+remote.SendUndoCommand();
+remote.SendUndoCommand();
